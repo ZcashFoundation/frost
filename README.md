c3fe237fe9093db2e7bfdfab30ce8c2d9fc6699a
Henry de Valence <hdevalence@hdevalence.ca>
2019-12-02 21:32:38 -0800

    Add readme, module layout

---
+++ README.md
@@ -0,0 +1,6 @@
+# redjubjub-zebra
+
+A minimal [Redjubjub][redjubjub] implementation for use in [Zebra][zebra].
+
+[redjubjub]: https://zips.z.cash/protocol/protocol.pdf#concretereddsa
+[zebra]: https://github.com/ZcashFoundation/zebra
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+#![cfg_attr(feature = "nightly", feature(external_doc))]
+#![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
+
+//! Docs require the `nightly` feature until RFC 1990 lands.
+
+mod public_key;
+mod secret_key;
+mod signature;
+mod error;
+
+
 #[cfg(test)]
 mod tests {
     #[test]
