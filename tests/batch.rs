2949f9a3f956829317ccc4f2d58045daac150d2b
Deirdre Connolly <deirdre@zfnd.org>
2021-02-21 22:07:12 -0500

    Upgrade rand, rand_core, and rand_chacha together

--- tests/batch.rs
+++ tests/batch.rs
@@ -4,13 +4,13 @@ use redjubjub::*;
 
 #[test]
 fn spendauth_batch_verify() {
-    let rng = thread_rng();
+    let mut rng = thread_rng();
     let mut batch = batch::Verifier::new();
     for _ in 0..32 {
-        let sk = SigningKey::<SpendAuth>::new(rng);
+        let sk = SigningKey::<SpendAuth>::new(&mut rng);
         let vk = VerificationKey::from(&sk);
         let msg = b"BatchVerifyTest";
-        let sig = sk.sign(rng, &msg[..]);
+        let sig = sk.sign(&mut rng, &msg[..]);
         batch.queue((vk.into(), sig, msg));
     }
     assert!(batch.verify(rng).is_ok());
@@ -18,13 +18,13 @@ fn spendauth_batch_verify() {
 
 #[test]
 fn binding_batch_verify() {
-    let rng = thread_rng();
+    let mut rng = thread_rng();
     let mut batch = batch::Verifier::new();
     for _ in 0..32 {
-        let sk = SigningKey::<Binding>::new(rng);
+        let sk = SigningKey::<Binding>::new(&mut rng);
         let vk = VerificationKey::from(&sk);
         let msg = b"BatchVerifyTest";
-        let sig = sk.sign(rng, &msg[..]);
+        let sig = sk.sign(&mut rng, &msg[..]);
         batch.queue((vk.into(), sig, msg));
     }
     assert!(batch.verify(rng).is_ok());
@@ -32,22 +32,22 @@ fn binding_batch_verify() {
 
 #[test]
 fn alternating_batch_verify() {
-    let rng = thread_rng();
+    let mut rng = thread_rng();
     let mut batch = batch::Verifier::new();
     for i in 0..32 {
         let item: batch::Item = match i % 2 {
             0 => {
-                let sk = SigningKey::<SpendAuth>::new(rng);
+                let sk = SigningKey::<SpendAuth>::new(&mut rng);
                 let vk = VerificationKey::from(&sk);
                 let msg = b"BatchVerifyTest";
-                let sig = sk.sign(rng, &msg[..]);
+                let sig = sk.sign(&mut rng, &msg[..]);
                 (vk.into(), sig, msg).into()
             }
             1 => {
-                let sk = SigningKey::<Binding>::new(rng);
+                let sk = SigningKey::<Binding>::new(&mut rng);
                 let vk = VerificationKey::from(&sk);
                 let msg = b"BatchVerifyTest";
-                let sig = sk.sign(rng, &msg[..]);
+                let sig = sk.sign(&mut rng, &msg[..]);
                 (vk.into(), sig, msg).into()
             }
             _ => unreachable!(),
@@ -59,28 +59,28 @@ fn alternating_batch_verify() {
 
 #[test]
 fn bad_batch_verify() {
-    let rng = thread_rng();
+    let mut rng = thread_rng();
     let bad_index = 4; // must be even
     let mut batch = batch::Verifier::new();
     let mut items = Vec::new();
     for i in 0..32 {
         let item: batch::Item = match i % 2 {
             0 => {
-                let sk = SigningKey::<SpendAuth>::new(rng);
+                let sk = SigningKey::<SpendAuth>::new(&mut rng);
                 let vk = VerificationKey::from(&sk);
                 let msg = b"BatchVerifyTest";
                 let sig = if i != bad_index {
-                    sk.sign(rng, &msg[..])
+                    sk.sign(&mut rng, &msg[..])
                 } else {
-                    sk.sign(rng, b"bad")
+                    sk.sign(&mut rng, b"bad")
                 };
                 (vk.into(), sig, msg).into()
             }
             1 => {
-                let sk = SigningKey::<Binding>::new(rng);
+                let sk = SigningKey::<Binding>::new(&mut rng);
                 let vk = VerificationKey::from(&sk);
                 let msg = b"BatchVerifyTest";
-                let sig = sk.sign(rng, &msg[..]);
+                let sig = sk.sign(&mut rng, &msg[..]);
                 (vk.into(), sig, msg).into()
             }
             _ => unreachable!(),
