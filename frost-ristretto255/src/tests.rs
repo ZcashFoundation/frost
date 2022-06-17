a0bf3c57f2bed1c8559153afdd08845d01a0168d
Deirdre Connolly <durumcrustulum@gmail.com>
2022-06-17 14:54:54 -0400

    Port frost-ristretto255 to frost-core (#57)
    * Start port to frost-core
    
    * Fix Signature from_bytes, frost-ristretto255 README / src/lib.rs doc test
    
    * Move frost-ristretto255 test vector tests to that crate
    
    * Uncomment proptest checks to exercise signature and verifying key (de)serialization
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- frost-ristretto255/src/tests.rs
+++ frost-ristretto255/src/tests.rs
@@ -1,59 +1,18 @@
-use curve25519_dalek::{constants::RISTRETTO_BASEPOINT_POINT, scalar::Scalar};
-
 use rand::thread_rng;
 
-use crate::frost::{self, *};
+use crate::*;
 
 mod vectors;
 
 use vectors::*;
 
-fn reconstruct_secret(
-    secret_shares: Vec<frost::keys::SecretShare>,
-) -> Result<Scalar, &'static str> {
-    let numshares = secret_shares.len();
-
-    if numshares < 1 {
-        return Err("No secret_shares provided");
-    }
-
-    let mut lagrange_coeffs: Vec<Scalar> = Vec::with_capacity(numshares as usize);
-
-    for i in 0..numshares {
-        let mut num = Scalar::one();
-        let mut den = Scalar::one();
-        for j in 0..numshares {
-            if j == i {
-                continue;
-            }
-            num *= Scalar::from(secret_shares[j].index as u64);
-            den *= Scalar::from(secret_shares[j].index as u64)
-                - Scalar::from(secret_shares[i].index as u64);
-        }
-        if den == Scalar::zero() {
-            return Err("Duplicate shares provided");
-        }
-        lagrange_coeffs.push(num * den.invert());
-    }
-
-    let mut secret = Scalar::zero();
-
-    for i in 0..numshares {
-        secret += lagrange_coeffs[i] * secret_shares[i].value.0;
-    }
-
-    Ok(secret)
-}
-
 /// This is testing that Shamir's secret sharing to compute and arbitrary
 /// value is working.
 #[test]
-fn check_share_generation() {
+fn check_share_generation_ristretto255_sha512() {
     let mut rng = thread_rng();
 
-    let secret = frost::keys::Secret::random(&mut rng);
-
-    let _ = RISTRETTO_BASEPOINT_POINT * secret.0;
+    let secret = frost::keys::Secret::<Ristretto255Sha512>::random(&mut rng);
 
     let secret_shares = frost::keys::generate_secret_shares(&secret, 5, 3, rng).unwrap();
 
@@ -61,7 +20,10 @@ fn check_share_generation() {
         assert_eq!(secret_share.verify(), Ok(()));
     }
 
-    assert_eq!(reconstruct_secret(secret_shares).unwrap(), secret.0)
+    assert_eq!(
+        frost::keys::reconstruct_secret::<Ristretto255Sha512>(secret_shares).unwrap(),
+        secret
+    )
 }
 
 #[test]
@@ -76,15 +38,20 @@ fn check_sign_with_test_vectors() {
         group_binding_factor_input,
         group_binding_factor,
         signature_shares,
-        signature,
+        signature_bytes,
     ) = parse_test_vectors();
+
+    type R = Ristretto255Sha512;
 
     ////////////////////////////////////////////////////////////////////////////
     // Key generation
     ////////////////////////////////////////////////////////////////////////////
 
     for key_package in key_packages.values() {
-        assert_eq!(key_package.public, key_package.secret_share.into());
+        assert_eq!(
+            *key_package.public(),
+            frost::keys::Public::from(*key_package.secret_share())
+        );
     }
 
     /////////////////////////////////////////////////////////////////////////////
@@ -97,13 +64,13 @@ fn check_sign_with_test_vectors() {
         let nonce_commitments = signer_commitments.get(&i).unwrap();
 
         assert_eq!(
-            frost::round1::NonceCommitment::from(nonces.hiding),
-            nonce_commitments.hiding
+            &frost::round1::NonceCommitment::from(nonces.hiding()),
+            nonce_commitments.hiding()
         );
 
         assert_eq!(
-            frost::round1::NonceCommitment::from(nonces.binding),
-            nonce_commitments.binding
+            &frost::round1::NonceCommitment::from(nonces.binding()),
+            nonce_commitments.binding()
         );
     }
 
@@ -112,7 +79,6 @@ fn check_sign_with_test_vectors() {
     /////////////////////////////////////////////////////////////////////////////
 
     let signer_commitments_vec = signer_commitments
-        .clone()
         .into_iter()
         .map(|(_, signing_commitments)| signing_commitments)
         .collect();
@@ -121,30 +87,30 @@ fn check_sign_with_test_vectors() {
 
     assert_eq!(signing_package.rho_preimage(), group_binding_factor_input);
 
-    let rho: Rho = (&signing_package).into();
+    let rho: frost::Rho<R> = (&signing_package).into();
 
     assert_eq!(rho, group_binding_factor);
 
-    let mut our_signature_shares: Vec<frost::round2::SignatureShare> = Vec::new();
+    let mut our_signature_shares: Vec<frost::round2::SignatureShare<R>> = Vec::new();
 
     // Each participant generates their signature share
     for index in signer_nonces.keys() {
-        let key_package = key_packages[index];
-        let nonces = signer_nonces[index];
+        let key_package = &key_packages[index];
+        let nonces = &signer_nonces[index];
 
         // Each participant generates their signature share.
-        let signature_share = frost::round2::sign(&signing_package, &nonces, &key_package).unwrap();
+        let signature_share = frost::round2::sign(&signing_package, nonces, key_package).unwrap();
 
         our_signature_shares.push(signature_share);
     }
 
     for sig_share in our_signature_shares.clone() {
-        assert_eq!(sig_share, signature_shares[&sig_share.index]);
+        assert_eq!(sig_share, signature_shares[sig_share.index()]);
     }
 
     let signer_pubkeys = key_packages
         .into_iter()
-        .map(|(i, key_package)| (i, key_package.public))
+        .map(|(i, key_package)| (i, *key_package.public()))
         .collect();
 
     let pubkey_package = frost::keys::PublicKeyPackage {
@@ -163,7 +129,7 @@ fn check_sign_with_test_vectors() {
         &signature_shares
             .values()
             .cloned()
-            .collect::<Vec<frost::round2::SignatureShare>>(),
+            .collect::<Vec<frost::round2::SignatureShare<R>>>(),
         &pubkey_package,
     );
 
@@ -172,7 +138,7 @@ fn check_sign_with_test_vectors() {
 
     // Check that the generated signature matches the test vector signature
     let group_signature = group_signature_result.unwrap();
-    assert_eq!(group_signature, signature);
+    assert_eq!(group_signature.to_bytes().to_vec(), signature_bytes);
 
     // Aggregate the FROST signature from our signature shares
     let group_signature_result =
@@ -183,5 +149,5 @@ fn check_sign_with_test_vectors() {
 
     // Check that the generated signature matches the test vector signature
     let group_signature = group_signature_result.unwrap();
-    assert_eq!(group_signature, signature);
+    assert_eq!(group_signature.to_bytes().to_vec(), signature_bytes);
 }
--- frost-ristretto255/tests/frost.rs
+++ frost-ristretto255/tests/frost.rs
@@ -1,25 +1,28 @@
 use std::{collections::HashMap, convert::TryFrom};
 
+use frost_ristretto255::*;
 use rand::thread_rng;
-
-use frost_ristretto255::frost;
 
 #[test]
 fn check_sign_with_dealer() {
     let mut rng = thread_rng();
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Key generation
+    ////////////////////////////////////////////////////////////////////////////
+
     let numsigners = 5;
     let threshold = 3;
-    let (shares, pubkeys) =
-        frost::keys::keygen_with_dealer(numsigners, threshold, &mut rng).unwrap();
+    let (shares, pubkeys) = keys::keygen_with_dealer(numsigners, threshold, &mut rng).unwrap();
 
     // Verifies the secret shares from the dealer
-    let key_packages: Vec<frost::keys::KeyPackage> = shares
+    let key_packages: Vec<keys::KeyPackage> = shares
         .into_iter()
-        .map(|share| frost::keys::KeyPackage::try_from(share).unwrap())
+        .map(|share| keys::KeyPackage::try_from(share).unwrap())
         .collect();
 
-    let mut nonces: HashMap<u16, Vec<frost::round1::SigningNonces>> = HashMap::new();
-    let mut commitments: HashMap<u16, Vec<frost::round1::SigningCommitments>> = HashMap::new();
+    let mut nonces: HashMap<u16, Vec<round1::SigningNonces>> = HashMap::new();
+    let mut commitments: HashMap<u16, Vec<round1::SigningCommitments>> = HashMap::new();
 
     ////////////////////////////////////////////////////////////////////////////
     // Round 1: generating nonces and signing commitments for each participant
@@ -28,7 +31,7 @@ fn check_sign_with_dealer() {
     for participant_index in 1..(threshold + 1) {
         // Generate one (1) nonce and one SigningCommitments instance for each
         // participant, up to _threshold_.
-        let (nonce, commitment) = frost::round1::preprocess(1, participant_index as u16, &mut rng);
+        let (nonce, commitment) = round1::preprocess(1, participant_index as u16, &mut rng);
         nonces.insert(participant_index as u16, nonce);
         commitments.insert(participant_index as u16, commitment);
     }
@@ -36,10 +39,10 @@ fn check_sign_with_dealer() {
     // This is what the signature aggregator / coordinator needs to do:
     // - decide what message to sign
     // - take one (unused) commitment per signing participant
-    let mut signature_shares: Vec<frost::round2::SignatureShare> = Vec::new();
+    let mut signature_shares: Vec<round2::SignatureShare> = Vec::new();
     let message = "message to sign".as_bytes();
     let comms = commitments.clone().into_values().flatten().collect();
-    let signing_package = frost::SigningPackage::new(comms, message.to_vec());
+    let signing_package = SigningPackage::new(comms, message.to_vec());
 
     ////////////////////////////////////////////////////////////////////////////
     // Round 2: each participant generates their signature share
@@ -51,11 +54,10 @@ fn check_sign_with_dealer() {
             .find(|key_package| *participant_index == key_package.index)
             .unwrap();
 
-        let nonces_to_use = nonces.get(participant_index).unwrap()[0];
+        let nonces_to_use = &nonces.get(participant_index).unwrap()[0];
 
         // Each participant generates their signature share.
-        let signature_share =
-            frost::round2::sign(&signing_package, &nonces_to_use, key_package).unwrap();
+        let signature_share = round2::sign(&signing_package, nonces_to_use, key_package).unwrap();
         signature_shares.push(signature_share);
     }
 
@@ -65,7 +67,7 @@ fn check_sign_with_dealer() {
     ////////////////////////////////////////////////////////////////////////////
 
     // Aggregate (also verifies the signature shares)
-    let group_signature_res = frost::aggregate(&signing_package, &signature_shares[..], &pubkeys);
+    let group_signature_res = aggregate(&signing_package, &signature_shares[..], &pubkeys);
 
     assert!(group_signature_res.is_ok());
 
