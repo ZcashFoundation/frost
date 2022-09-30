a11b7d60d3cf868a8e5e7d4c9b219743f12e9342
Conrado Gouvea <conradoplg@gmail.com>
2022-09-30 17:11:57 -0300

    make batch testing generic; remove duplicated Ristretto255 implementation

--- frost-p256/tests/frost.rs
+++ frost-p256/tests/frost.rs
@@ -7,3 +7,21 @@ fn check_sign_with_dealer() {
 
     frost_core::tests::check_sign_with_dealer::<P256Sha256, _>(rng);
 }
+
+// TODO: re-enable after batch is changed to work with big-endian scalars
+// #[test]
+#[allow(unused)]
+fn check_batch_verify() {
+    let rng = thread_rng();
+
+    frost_core::tests::batch::batch_verify::<P256Sha256, _>(rng);
+}
+
+// TODO: re-enable after batch is changed to work with big-endian scalars
+// #[test]
+#[allow(unused)]
+fn check_bad_batch_verify() {
+    let rng = thread_rng();
+
+    frost_core::tests::batch::bad_batch_verify::<P256Sha256, _>(rng);
+}
