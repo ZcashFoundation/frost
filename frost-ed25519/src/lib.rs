9514e7688e470e0ae8664a12133fdf6589c3c9aa
Conrado Gouvea <conrado@zfnd.org>
2022-12-15 06:38:53 -0300

    add check for canonical point encodings where needed, and tests (#193)
    * add check for canonical point encodings where needed, and tests
    
    * remove unneeded 'as' keywords
    
    * fix after syncing with main
    
    * pin curve25519-dalek for now due to breaking changes
    
    Co-authored-by: Deirdre Connolly <durumcrustulum@gmail.com>

--- frost-ed25519/src/lib.rs
+++ frost-ed25519/src/lib.rs
@@ -55,6 +55,15 @@ impl Group for Ed25519Group {
                 if point == Self::identity() {
                     Err(GroupError::InvalidIdentityElement)
                 } else if point.is_torsion_free() {
+                    // At this point we should reject points which were not
+                    // encoded canonically (i.e. Y coordinate >= p).
+                    // However, we don't allow non-prime order elements,
+                    // and that suffices to also reject non-canonical encodings
+                    // per https://eprint.iacr.org/2020/1244.pdf:
+                    //
+                    // > There are 19 elliptic curve points that can be encoded in a non-canonical form.
+                    // > (...) Among these points there are 2 points of small order and from the
+                    // > remaining 17 y-coordinates only 10 decode to valid curve points all of mixed order.
                     Ok(point)
                 } else {
                     Err(GroupError::InvalidNonPrimeOrderElement)
