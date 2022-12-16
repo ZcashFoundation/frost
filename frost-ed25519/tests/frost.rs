e43f65120865a14a962745876d9d844e09849a90
Conrado Gouvea <conradoplg@gmail.com>
2022-12-16 18:33:16 -0300

    add ed25519 interoperability test

--- frost-ed25519/tests/frost.rs
+++ frost-ed25519/tests/frost.rs
@@ -1,20 +1,50 @@
-use curve25519_dalek::{edwards::EdwardsPoint, traits::Identity};
 use frost_core::{Ciphersuite, Group, GroupError};
 use frost_ed25519::*;
+
+use curve25519_dalek::{edwards::EdwardsPoint, traits::Identity};
+use ed25519_dalek::Verifier;
 use rand::thread_rng;
+
+fn verify_signature(
+    msg: &[u8],
+    group_signature: frost_core::Signature<Ed25519Sha512>,
+    group_pubkey: frost_core::VerifyingKey<Ed25519Sha512>,
+) {
+    let sig = {
+        let bytes: [u8; 64] = group_signature.to_bytes();
+        ed25519_dalek::Signature::from(bytes)
+    };
+    let pub_key = {
+        let bytes = group_pubkey.to_bytes();
+        ed25519_dalek::PublicKey::from_bytes(&bytes).unwrap()
+    };
+    // Check that signature validation has the expected result.
+    assert!(pub_key.verify(msg, &sig).is_ok());
+}
 
 #[test]
 fn check_sign_with_dealer() {
     let rng = thread_rng();
 
-    frost_core::tests::check_sign_with_dealer::<Ed25519Sha512, _>(rng);
+    // For the interoperability test it's better to test with multiple signatures
+    for _ in 0..256 {
+        let (msg, group_signature, group_pubkey) =
+            frost_core::tests::check_sign_with_dealer::<Ed25519Sha512, _>(rng.clone());
+
+        // Check that the threshold signature can be verified by the `ed25519_dalek` crate
+        // public key (interoperability test)
+        verify_signature(&msg, group_signature, group_pubkey);
+    }
 }
 
 #[test]
 fn check_sign_with_dkg() {
     let rng = thread_rng();
 
-    frost_core::tests::check_sign_with_dkg::<Ed25519Sha512, _>(rng);
+    let (msg, group_signature, group_pubkey) =
+        frost_core::tests::check_sign_with_dkg::<Ed25519Sha512, _>(rng);
+
+    verify_signature(&msg, group_signature, group_pubkey);
 }
 
 #[test]
