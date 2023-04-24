6b80689019b2f9e58c1fe480e2e30916875cec76
Conrado Gouvea <conrado@zfnd.org>
2023-04-24 12:13:27 -0300

    Update frost-core/src/frost/keys/repairable.rs

--- frost-core/src/frost/keys/repairable.rs
+++ frost-core/src/frost/keys/repairable.rs
@@ -1,6 +1,6 @@
 //! Repairable Threshold Scheme
 //!
-//! Implements the Repairable Threshold Scheme (RTS) from [https://eprint.iacr.org/2017/1155].
+//! Implements the Repairable Threshold Scheme (RTS) from <https://eprint.iacr.org/2017/1155>.
 //! The RTS is used to help a signer (participant) repair their lost share. This is achieved
 //! using a subset of the other signers know here as `helpers`.
 
