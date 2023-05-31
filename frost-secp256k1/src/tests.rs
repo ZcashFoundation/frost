10a4baf970d85dc35c6dab1c74b12e71077bc676
natalie <n.l.eskinazi@gmail.com>
2023-05-31 13:04:52 +0100

    Rename signing_tests to ciphersuite_generic_tests to match frost-core (#364)

--- frost-secp256k1/src/tests.rs
+++ frost-secp256k1/src/tests.rs
@@ -1,12 +1,12 @@
 #[cfg(test)]
 mod batch_tests;
 #[cfg(test)]
+mod ciphersuite_generic_tests;
+#[cfg(test)]
 mod coefficient_commitment_tests;
 #[cfg(test)]
 mod deserialize_tests;
 #[cfg(test)]
 mod proptests;
-#[cfg(test)]
-mod signing_tests;
 #[cfg(test)]
 mod vss_commitment_tests;
