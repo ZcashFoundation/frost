7358b667ca0a5c84f31889a083f566acdcd5e983
Conrado Gouvea <conrado@zfnd.org>
2023-06-23 11:22:33 -0300

    expand serde tests (#409)
    using samples in serde tests

--- frost-ed448/src/lib.rs
+++ frost-ed448/src/lib.rs
@@ -14,6 +14,9 @@ use sha3::{
 
 use frost_core::frost;
 
+#[cfg(feature = "serde")]
+use frost_core::serde;
+
 #[cfg(test)]
 mod tests;
 
@@ -142,6 +145,8 @@ const CONTEXT_STRING: &str = "FROST-ED448-SHAKE256-v11";
 
 /// An implementation of the FROST(Ed448, SHAKE256) ciphersuite.
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(crate = "self::serde"))]
 pub struct Ed448Shake256;
 
 impl Ciphersuite for Ed448Shake256 {
--- frost-p256/src/lib.rs
+++ frost-p256/src/lib.rs
@@ -15,6 +15,9 @@ use sha2::{Digest, Sha256};
 
 use frost_core::frost;
 
+#[cfg(feature = "serde")]
+use frost_core::serde;
+
 #[cfg(test)]
 mod tests;
 
@@ -167,6 +170,8 @@ const CONTEXT_STRING: &str = "FROST-P256-SHA256-v11";
 
 /// An implementation of the FROST(P-256, SHA-256) ciphersuite.
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(crate = "self::serde"))]
 pub struct P256Sha256;
 
 impl Ciphersuite for P256Sha256 {
