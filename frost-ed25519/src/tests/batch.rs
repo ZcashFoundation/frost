fcd0e31e6ba753d43fee4c7e61311e6ff5a8be58
Conrado Gouvea <conrado@zfnd.org>
2023-09-02 02:13:51 -0300

    return error when validating an empty batch (#487)
    * return error when validating an empty batch
    
    * reuse n

--- frost-ed25519/src/tests/batch.rs
+++ frost-ed25519/src/tests/batch.rs
@@ -15,3 +15,10 @@ fn check_bad_batch_verify() {
 
     frost_core::tests::batch::bad_batch_verify::<Ed25519Sha512, _>(rng);
 }
+
+#[test]
+fn empty_batch_verify() {
+    let rng = thread_rng();
+
+    frost_core::tests::batch::empty_batch_verify::<Ed25519Sha512, _>(rng);
+}
