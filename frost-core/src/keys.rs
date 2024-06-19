647da3515c2f076295885cc715c034da4e262fc1
Conrado Gouvea <conrado@zfnd.org>
2024-06-19 17:04:19 -0300

    core: remove MulAssign impl for Scalar (#626)

--- frost-core/src/keys.rs
+++ frost-core/src/keys.rs
@@ -582,7 +582,7 @@ fn evaluate_polynomial<C: Ciphersuite>(
     let ell_scalar = identifier;
     for coeff in coefficients.iter().skip(1).rev() {
         value = value + *coeff;
-        value *= ell_scalar;
+        value = ell_scalar * value;
     }
     value = value
         + *coefficients
